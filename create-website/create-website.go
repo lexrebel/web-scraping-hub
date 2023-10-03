901aa175f32d121c2b643a0fa1cc3de75c779acc
Alex Rebello <lexrebello@gmail.com>
2023-10-03 05:17:20 -0300

    create website function

--- create-website/create-website.go
+++ create-website/create-website.go
@@ -11,16 +11,26 @@ import (
 )
 
 const (
-	WEBSITE_KIND = "websites"
+	COLLECTION = "websites"
 )
 
-type website struct {
-	ID        *datastore.Key `json:"id" datastore:"-"`
-	Title     string         `json:"title"`
-	CreatedAt time.Time      `json:"createdAt"`
-	UpdatedAt time.Time      `json:"updatedAt"`
+// WebsiteDTO represents the input data for creating a website.
+type WebsiteDTO struct {
+	URL             string   `json:"url"`             // URL of the website to scrape
+	Name            string   `json:"name"`            // Name or identifier for the website
+	RowSelector     string   `json:"rowSelector"`     // Selector for rows on the webpage
+	ColumnSelectors []string `json:"columnSelectors"` // List of selectors for columns within each row
 }
 
+// Website represents the structure for storing website data in Datastore.
+type Website struct {
+	ID *datastore.Key `json:"id" datastore:"-"`
+	*WebsiteDTO
+	CreatedAt time.Time `json:"createdAt"` // Timestamp for when the website entry was created
+	UpdatedAt time.Time `json:"updatedAt"` // Timestamp for when the website entry was last updated
+}
+
+// CreateWebsite is the HTTP handler function for creating a new website.
 func CreateWebsite(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
@@ -28,7 +38,6 @@ func CreateWebsite(w http.ResponseWriter, r *http.Request) {
 	}
 
 	ctx := context.Background()
-
 	client, err := createClient(ctx)
 	if err != nil {
 		log.Printf("ERROR creating Datastore client, err: %v", err)
@@ -36,17 +45,21 @@ func CreateWebsite(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var website website
-	if err := json.NewDecoder(r.Body).Decode(&website); err != nil {
+	var websiteDTO WebsiteDTO
+	if err := json.NewDecoder(r.Body).Decode(&websiteDTO); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
+	// Create an entity with the provided data
+	website := &Website{WebsiteDTO: &websiteDTO}
+
+	// Set the timestamps for creation and update
 	now := time.Now()
 	website.CreatedAt = now
 	website.UpdatedAt = now
 
-	key := datastore.IncompleteKey(WEBSITE_KIND, nil)
+	key := datastore.IncompleteKey(COLLECTION, nil)
 	key, err = client.Put(ctx, key, &website)
 	if err != nil {
 		log.Printf("ERROR setting a website: %s", err)
@@ -59,6 +72,7 @@ func CreateWebsite(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(website)
 }
 
+// createClient creates a new Datastore client.
 func createClient(ctx context.Context) (*datastore.Client, error) {
 	client, err := datastore.NewClient(ctx, "web-scraping-hub")
 	if err != nil {
